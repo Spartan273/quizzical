dd4635fc90bfb69fda08c6b27c55060d96564b47
Spartan273 <sami273@live.ca>
2023-03-05 14:23:39 -0500

    create the start page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <></>
+    <main className="container">
+      <div>
+        <h1>Quizzical</h1>
+        <p className="description">The trivia game</p>
+        <button>Start quiz</button>
+      </div>
+    </main>
   );
 }
 
